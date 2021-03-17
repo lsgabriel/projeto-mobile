ac14d1d06d1b96a34fcb674dbbb04503cf26d7de
JPLeopolino <leopoldino26@gmail.com>
2021-03-17 00:47:46 -0300

    Criando icone de favoritos

--- ProjetoSalao/Frontend/AppSalao/components/ProfessionalListItem.js
+++ ProjetoSalao/Frontend/AppSalao/components/ProfessionalListItem.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 
 const ProfessionalListItem = ({item})=>{
+    const navigation = useNavigation();
     return(
         <View style={styles.main}>
+            <Icon
+                type='material-community' name={item.Pfav ? 'star' : 'star-outline'} color={item.Pfav ? '#FFF' : '#a32bab'} containerStyle={styles.star} size={28}
+            />
             <Image
                 style={styles.img}
                 source={{uri:item.Ppic}}
@@ -13,7 +18,9 @@ const ProfessionalListItem = ({item})=>{
                 <Text style={styles.title}>{item.Pname}</Text>
                 <Text style={styles.desc}>{item.Pdesc}</Text>
             </View>
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button}
+                onPress={()=>{ navigation.navigate('ProfessionalsDesc', item) }}
+            >                
                 <Icon
                     type='font-awesome' name='calendar' color='#CA33D2' size={30}
                 />
@@ -30,10 +37,17 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         marginVertical: 4,
         backgroundColor: '#FFF',
-
+        position: 'relative',
         borderWidth: 1.5,
         borderColor: '#832189',
         borderRadius: 15,
+    },
+    star:{
+        position: 'absolute',
+        zIndex: 99,
+        backgroundColor: '#832189',
+        borderRadius: 13,
+
     },
     img:{
         flex: 1,
