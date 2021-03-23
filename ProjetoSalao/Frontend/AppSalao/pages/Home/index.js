92af78722758ebb7ca3e9e0268c3ff0ace36ac82
JPLeopolino <leopoldino26@gmail.com>
2021-03-23 00:04:36 -0300

    Criando Estilos e botões

--- ProjetoSalao/Frontend/AppSalao/pages/Home/index.js
+++ ProjetoSalao/Frontend/AppSalao/pages/Home/index.js
@@ -1,62 +1,138 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
-import { Header, Icon, SearchBar } from 'react-native-elements';
+import { View, Text, Image, TouchableOpacity, TouchableWithoutFeedback, TouchableHighlight } from 'react-native';
+import { Header, SearchBar } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import data from '../Professionals/data';
+import { LinearGradient } from 'expo-linear-gradient';
 import styles from './styles';
 
 const Home = () => {
 
-    return <View style = {styles.main}>
-            {/* <Header 
-                style = {styles.header}
-            /> */}
-            <Image
-              source = {{uri:data[1].Ppic}}
-              style = {styles.img}
-            />
-            <Text
-                style = {styles.mainText}
-            >
-                Olá, {data[1].Pname}
-            </Text>
+    return (
+        <LinearGradient
+            // Background Linear Gradient
+            colors={['#FFDEFF', '#C833CA']}
+            style={styles.mainContainer}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}
+        >
+            <View style={styles.main}>
+                {/* <Header 
+                        style = {styles.header}
+                    /> */}
+                <TouchableWithoutFeedback
+                    // onPress={navigator.navigate('Perfil')}
+                    onPress={()=>console.log('teste')}
+                >
+                    <Image
+                        source={{ uri: data[1].Ppic }}
+                        style={styles.img}
+                    />
+                </TouchableWithoutFeedback>
+                <View style={styles.mainTextContainer}>
+                    <Text
+                        style={styles.subMainText}
+                    >
+                        Olá,
+                    </Text>
+                    <Text
+                        style={styles.mainText}
+                    >
+                        {data[1].Pname}
+                    </Text>
+                </View>
 
-            <SearchBar 
-                placeholder="Buscar um profissional" 
-                style = {styles.SearchBar} 
-            />
+                <SearchBar
+                    placeholder="Buscar um profissional"
+                    style={styles.search}
+                    containerStyle={styles.searchBar}
+                    inputContainerStyle={styles.searchBarInputContainer}
+                    inputStyle={styles.searchBarInput}
+                    round={true}
+                    placeholderTextColor='#FFF'
+                    searchIcon={
+                        <Icon 
+                            name='search'
+                            size={24}
+                            color='#FFF'
+                        />
+                    }
+                    clearIcon={
+                        <Icon 
+                            name='close'
+                            size={24}
+                            color='#FFF'
+                        />
+                    }
+                    lightTheme={false}
+                />
 
-            <Text style = {styles.dividerText}
-            >ou</Text>
+                <Text style={styles.dividerText}
+                >ou</Text>
 
-            <View style = {styles.containerText}>
-                <View style = {styles.line}/>
-                <Text style = {styles.lineText}>
-                    Escolha um serviço
-                </Text>
-                <View style = {styles.line}/>
-            </View>
+                <View style={styles.containerText}>
+                    <View style={styles.line} />
+                    <Text style={styles.lineText}>
+                        Escolha um serviço
+                        </Text>
+                    <View style={styles.line} />
+                </View>
 
-            <View>
-                <TouchableOpacity>
-                    <Icon />
-                </TouchableOpacity>
+                <View style={styles.servicesContainer}>
+                    
+                    <TouchableOpacity style={styles.servicesItem}>
+                        <Icon
+                            name='email'
+                            size={45}
+                            style={styles.servicesIcon}
+                            color='#832189'
+                        />
+                        <Text style={styles.servicesLabel}>
+                            Manicure
+                        </Text>
+                    </TouchableOpacity>
+                                        
+                    <TouchableOpacity style={styles.servicesItem}>
+                        <Icon
+                            name='email'
+                            size={45}
+                            style={styles.servicesIcon}
+                            color='#832189'
+                        />
+                        <Text style={styles.servicesLabel}>
+                            Maquiagem
+                        </Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity>
-                    <Icon />
-                </TouchableOpacity>
+                    <TouchableOpacity style={styles.servicesItem}>
+                        <Icon
+                            name='email'
+                            size={45}
+                            style={styles.servicesIcon}
+                            color='#832189'
+                        />
+                        <Text style={styles.servicesLabel}>
+                            Massagem
+                        </Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity>
-                    <Icon />
-                </TouchableOpacity>
+                    <TouchableOpacity style={styles.servicesItem}>
+                        <Icon
+                            name='email'
+                            size={45}
+                            style={styles.servicesIcon}
+                            color='#832189'
+                        />
+                        <Text style={styles.servicesLabel}>
+                            Cabelereira
+                        </Text>
+                    </TouchableOpacity>
+
+                </View>
                 
-                <TouchableOpacity>
-                    <Icon />
-                </TouchableOpacity>
             </View>
-
-
-
-        </View>
+        </LinearGradient>
+    );
 }
 
 export default Home;
--- ProjetoSalao/Frontend/AppSalao/pages/Home/styles.js
+++ ProjetoSalao/Frontend/AppSalao/pages/Home/styles.js
@@ -1,25 +1,57 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+    container:{
+        height:'100%',
+        width: '100%',
+    },
+    main:{
+        alignItems: 'center',
+    },
     img:{
-        marginTop: 40,
-        height: 250,
-        width: 250,
+        
+        marginTop: 60,
+        height: 200,
+        width: 200,
         borderRadius: 300,
         borderWidth: 4,
         borderColor: '#832189',
     },
-    main:{
-        backgroundColor: '#000',
-        alignItems: 'center',
+    mainTextContainer:{
+        marginTop: 15,
+    },
+    subMainText:{
+        fontSize: 15,
+        color: '#FFF',
+        fontWeight: 'bold',
     },
     mainText:{
         color: 'white',
-        fontSize: 30,
+        fontSize: 45,
         textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    searchBar:{
+        marginVertical: 20,
+        borderBottomWidth: 0,
+        borderTopWidth: 0,
+        width: '90%',
+        borderRadius: 0,
+        backgroundColor: '#F128F5',
+        borderRadius: 300,
+    },
+    searchBarInput:{
+        
+        // borderRadius: 300,
+    },
+    searchBarInputContainer:{
+        backgroundColor: 'transparent'
+        
     },
     dividerText:{
+        fontSize: 30,
         color: 'white',
+        marginBottom: 15,
     },
     containerText:{
         flexDirection: 'row',
@@ -27,11 +59,38 @@ const styles = StyleSheet.create({
     },
     line:{
         height: 5,
-        width: 60,
+        width: '15%',
         backgroundColor: 'white',
+        borderRadius: 50,
     },
     lineText:{
-        color: 'white'
+        fontWeight: 'bold',
+        fontSize: 30,
+        color: 'white',
+        marginHorizontal: 20,
+    },
+    servicesContainer:{
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        
+    },
+    servicesItem:{
+        // flexBasis: '45%',
+        margin: 10,
+        backgroundColor: '#FFF',
+        height: 100,
+        width: 100,
+        borderRadius: 300,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    servicesIcons:{
+    },
+    servicesLabel:{
+        fontSize: 14,
+        color: '#832189',
+        fontWeight: 'bold',
+        letterSpacing: 1,
     }
 
 })
