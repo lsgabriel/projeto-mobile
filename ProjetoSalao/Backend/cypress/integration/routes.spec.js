a0b933833b777e27a219140831525712e0689cc8
Gabriel Henrique <souzatrindade28@gmail.com>
2021-05-26 21:23:47 -0300

    commit

--- ProjetoSalao/Backend/cypress/integration/routes.spec.js
+++ ProjetoSalao/Backend/cypress/integration/routes.spec.js
@@ -3,7 +3,7 @@
 describe('Should verify the routes returns', () => {
     it('Should have the message `Projeto em execução!`', () => {
         cy.request({
-            url: 'http://localhost:3001'
+            url: 'http://localhost:3000/'
         }).then((resp) => {
             expect(resp.body).to.eq('Projeto em execução!');
             expect(resp.status).to.eq(200);
